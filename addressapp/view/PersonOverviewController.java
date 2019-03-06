1e99043be0e8a790e17d0680dca503e1d77d8188
Guoli Hao <ghao@cpp.edu>
2019-03-05 23:56:24 -0800

    first commit with GUI

---
+++ addressapp/view/PersonOverviewController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addressapp.view;
+
+import addressapp.MainApp;
+import addressapp.model.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author Howard
+ */
+public class PersonOverviewController {
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+    
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+    
+    private MainApp mainApp;
+
+    @FXML
+    private void initialize() {
+        //Initialize the person table with the two columns.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        
+    }
+    
+    /**
+     * Method called by main class
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+    this.mainApp = mainApp;
+    personTable.setItems(mainApp.getList());
+    }
+}
